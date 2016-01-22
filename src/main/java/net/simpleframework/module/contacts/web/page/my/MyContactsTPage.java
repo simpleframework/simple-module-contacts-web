5bd3009c9d4425e82659e11f62a8b928b19b5c11
CHENK <CHENK@CHENK-PC>
2016-01-22 12:36:48 +0800

    Signed-off-by: CHENK <CHENK@CHENK-PC>

--- src/main/java/net/simpleframework/module/contacts/web/page/my/MyContactsTPage.java
+++ src/main/java/net/simpleframework/module/contacts/web/page/my/MyContactsTPage.java
@@ -3,6 +3,9 @@ package net.simpleframework.module.contacts.web.page.my;
 import net.simpleframework.mvc.PageParameter;
 import net.simpleframework.mvc.common.element.ElementList;
 import net.simpleframework.mvc.common.element.LinkButton;
+import net.simpleframework.mvc.component.ui.pager.TablePagerBean;
+import net.simpleframework.mvc.component.ui.pager.TablePagerColumn;
+import net.simpleframework.mvc.component.ui.pager.db.AbstractDbTablePagerHandler;
 import net.simpleframework.mvc.template.AbstractTBTemplatePage;
 
 /**
@@ -17,8 +20,17 @@ public class MyContactsTPage extends AbstractTBTemplatePage {
 	@Override
 	protected void onForward(final PageParameter pp) throws Exception {
 		super.onForward(pp);
-
 		pp.addImportCSS(MyContactsTPage.class, "/my-contacts.css");
+
+		// 添加表格
+		addTablePagerBean(pp);
+	}
+
+	protected TablePagerBean addTablePagerBean(final PageParameter pp) {
+		final TablePagerBean tablePager = (TablePagerBean) super.addTablePagerBean(pp,
+				"MyContactsTPage_tbl", MyContactsTbl.class).setContainerId("idMyContactsTPage_tbl");
+		tablePager.addColumn(TablePagerColumn.OPE(70));
+		return tablePager;
 	}
 
 	@Override
@@ -30,8 +42,11 @@ public class MyContactsTPage extends AbstractTBTemplatePage {
 	protected String toContentHTML(final PageParameter pp) {
 		final StringBuilder sb = new StringBuilder();
 		sb.append("<div class='cc'>");
-		sb.append("<div id='idMyContactsTPage_tbl'></div>");
+		sb.append(" <div id='idMyContactsTPage_tbl'></div>");
 		sb.append("</div>");
 		return sb.toString();
+	}
+
+	public static class MyContactsTbl extends AbstractDbTablePagerHandler {
 	}
 }
