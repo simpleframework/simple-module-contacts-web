a045dc35f3c849de740c7eb86dd95035625e0fa2
CHENK <CHENK@CHENK-PC>
2016-01-22 12:03:28 +0800

    Signed-off-by: CHENK <CHENK@CHENK-PC>

--- src/main/java/net/simpleframework/module/contacts/web/page/my/MyContactsTPage.java
+++ src/main/java/net/simpleframework/module/contacts/web/page/my/MyContactsTPage.java
@@ -1,6 +1,9 @@
 package net.simpleframework.module.contacts.web.page.my;
 
-import net.simpleframework.mvc.template.AbstractTemplatePage;
+import net.simpleframework.mvc.PageParameter;
+import net.simpleframework.mvc.common.element.ElementList;
+import net.simpleframework.mvc.common.element.LinkButton;
+import net.simpleframework.mvc.template.AbstractTBTemplatePage;
 
 /**
  * Licensed under the Apache License, Version 2.0
@@ -9,5 +12,26 @@ import net.simpleframework.mvc.template.AbstractTemplatePage;
  *         https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
-public class MyContactsTPage extends AbstractTemplatePage {
+public class MyContactsTPage extends AbstractTBTemplatePage {
+
+	@Override
+	protected void onForward(final PageParameter pp) throws Exception {
+		super.onForward(pp);
+
+		pp.addImportCSS(MyContactsTPage.class, "/my-contacts.css");
+	}
+
+	@Override
+	public ElementList getRightElements(final PageParameter pp) {
+		return ElementList.of(LinkButton.addBtn());
+	}
+
+	@Override
+	protected String toContentHTML(final PageParameter pp) {
+		final StringBuilder sb = new StringBuilder();
+		sb.append("<div class='cc'>");
+		sb.append("<div id='idMyContactsTPage_tbl'></div>");
+		sb.append("</div>");
+		return sb.toString();
+	}
 }
