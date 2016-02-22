3972f5ecafade116a86a9f2282dceecd03a1ddd4
CHENK <CHENK@CHENK-PC>
2016-02-22 16:11:33 +0800

    Signed-off-by: CHENK <CHENK@CHENK-PC>

---
+++ src/main/java/net/simpleframework/module/contacts/web/IContactsWebContext.java
@@ -0,0 +1,13 @@
+package net.simpleframework.module.contacts.web;
+
+import net.simpleframework.module.contacts.IContactsContext;
+
+/**
+ * Licensed under the Apache License, Version 2.0
+ * 
+ * @author 陈侃(cknet@126.com, 13910090885)
+ *         https://github.com/simpleframework
+ *         http://www.simpleframework.net
+ */
+public interface IContactsWebContext extends IContactsContext {
+}
